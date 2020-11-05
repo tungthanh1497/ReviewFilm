1ef1e5cd3d953784fede87a6d02b038d6db4e02f
tungtt <tungthanh1497@gmail.com>
2020-11-05 13:10:30 +0700

    init main screen
    Signed-off-by: tungtt <tungthanh1497@gmail.com>

--- app/src/main/java/com/tungtt/reviewfilm/MainActivity.java
+++ app/src/main/java/com/tungtt/reviewfilm/MainActivity.java
@@ -1,14 +1,29 @@
 package com.tungtt.reviewfilm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tungtt.reviewfilm.screens.main.MainFragment;
+import com.tungtt.reviewfilm.utils.ActivityUtil;
+import com.tungtt.reviewfilm.utils.Constant;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity instance;
+
+    public static MainActivity getInstance() {
+        if (instance == null) {
+            instance = new MainActivity();
+        }
+        return instance;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance = this;
+        ActivityUtil.addScreen(MainFragment.newInstance(), Constant.SCREEN_TAG.MAIN);
     }
 }
